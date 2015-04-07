c619a6a9bb9d90108e4d7f58f922e9be1a88e5bc
Tom Fuertes <tomfuertes@gmail.com>
2015-04-07 17:35:15 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# timpl
+
+Inspied by 
+ 
+* https://github.com/sindresorhus/multiline
+* https://github.com/premasagar/tim
+
+tldr multiline || strings with optional data templating 
+
+## Usage
+
+```js
+window.timpl('one: {{one}}', {one: 1}); // ==> one: 1
+
+window.timpl('two: {{0}}', [2]); // ==> two: 2
+
+window.timpl(function () {/*
+  foo: bar
+*/}); // ==> foo: bar
+
+window.timpl(function () {/*
+  foo: {{bar}}
+*/}, {bar: 'foo'}); // ==> foo: foo
+```
---
+++ test.js
@@ -0,0 +1,75 @@
+var assert = require('assert');
+var timpl = require('./timpl').timpl;
+
+it('should support simple strings + data', function () {
+  var actual = timpl('one: {{one}}', {one: 1}); // ==> one: 1
+  assert.equal(actual, 'one: 1');
+});
+
+it('should support simple strings + arrays', function () {
+  var actual = timpl('two: {{0}}', [2]); // ==> two: 2
+  assert.equal(actual, 'two: 2');
+});
+
+it('should support multiline strings + data', function () {
+  var actual = timpl(function () {/*
+  foo: {{bar}}
+*/}, {bar: 'foo'}); // ==> foo: foo
+  assert.equal(actual, 'foo: foo');
+});
+
+
+it('should support multiline comments', function () {
+  var actual = timpl(function(){/*
+<!doctype html>
+<html>
+  <body>
+    <h1>Hello world!</h1>
+  </body>
+</html>
+  */});
+  var expected = '<!doctype html>\n<html>\n  <body>\n    <h1>Hello world!</h1>\n  </body>\n</html>';
+  assert.equal(actual, expected);
+});
+
+it('should match when comment starts with `/*!`', function () {
+  var actual = timpl(function(){/*!
+foo
+  */});
+  var expected = 'foo';
+  assert.equal(actual, expected);
+});
+
+it('should match when comment starts with `/*@preserve`', function () {
+  var actual = timpl(function(){/*@preserve
+foo
+  */});
+  var expected = 'foo';
+  assert.equal(actual, expected);
+});
+
+it('should match when comment starts with `/*!@preserve`', function () {
+  var actual = timpl(function(){/*!@preserve
+foo
+  */});
+  var expected = 'foo';
+  assert.equal(actual, expected);
+});
+
+it('should wipe leading and trailing empty lines', function () {
+  var actual = timpl(function(){/*
+foo
+  */});
+  var expected = 'foo';
+  assert.equal(actual, expected);
+});
+
+it('should throw if it can\'t match comment contents', function () {
+  assert.throws(function () {
+    timpl(function(){});
+  });
+
+  assert.throws(function () {
+    timpl(function(){/**/});
+  });
+});
